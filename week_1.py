7afb5e2ed2c48e92062d838fa59dfbd6c3e0bebd
itsTofan <106501521+itsTofan@users.noreply.github.com>
2023-06-23 14:33:50 +0930

    pyhton with OS

---
+++ week_1.py
@@ -0,0 +1,34 @@
+#Python Operation with OS
+#Operation system is the software that manages everthing that goes on in the computer
+#Kernel is the main core of OS, talk to hardware
+#user space
+#this course focus on manging files.
+#Windows, mac os, linux
+#linus distribution - ubuntu, debian, red hat
+#unix develop by belllabs
+#pyhton external module - PyPi python package index
+#pip - command line tool - cross platform tools use to install, update and remove external module
+#pip install requests to install external module dealing with web services
+
+import requests
+#ger function process website and response object has it content
+#response = requests.get("http://www.google.com")
+#print(len(response.text))
+
+#interpreted language - python, ruby, javascript, bash, powershell 
+#compiler translate a code to machine level language to specific underling os
+#compile programming language c, c++, Go, Rust
+#relly on intrepreter
+
+#run python on linux command line
+#python 3
+#cat to show content of file cat hello_world.py
+#pyhton3 hello_world.py - to run script
+#shebang - command to use to execute script
+#nano editior in linux
+#!/user/bin/env python3
+#make file executeable with CHMOD
+#chmod +x hello_world.py
+#run with ./hello_world.py
+
+
