ab8626a9522ba0d8d7b2b42f590df9477d65f4e0
itsTofan <106501521+itsTofan@users.noreply.github.com>
2023-07-04 09:42:09 +0930

    advance regex, repetition qualifier, extract string, split & replace

--- Week 3/week_3.py
+++ Week 3/week_3.py
@@ -69,3 +69,57 @@ pattern = r"^[a-zA-Z_][a-zA-Z0-9_]*$"
 print(re.search(pattern, "_this_is_valid_variable_name"))
 print(re.search(pattern, "this isn't a valid variable"))
 print(re.search(pattern, "my_ variable1"))
+
+#advance regular expression
+#Caputuring groups - portion of the parten that are enclosed in parantheses
+result = re.search(r"^(\w*), (\w*)$", "Lovelace, Ada") #\w will match letters, number, underscrore
+print(result)
+print(result.group())
+print(result[0])
+print(result[1])
+print(result[2])
+print("{} {}".format(result[2], result[1]))
+
+def rearrenge_name(name):
+    result = re.search(r"^([\w .-]*), ([\w .-]*)$", name)
+    if result is None:
+        return name
+    return "{} {}".format(result[2], result[1])
+
+print(rearrenge_name("Lovelace, Ada"))
+print(rearrenge_name("Ritchie, Dennis"))
+print(rearrenge_name("Hopper, Grace M."))
+
+#Repetition Qualifier
+#match any string exactly 5 letter
+print(re.search(r"[a-zA-Z]{5}", "a ghost"))
+print(re.search(r"[a-zA-Z]{5}", "a scary ghost"))
+print(re.findall(r"[a-zA-Z]{5}", "a scary ghost appeared"))
+print(re.findall(r"\b[a-zA-Z]{5}\b", "A scary ghost"))
+print(re.findall(r"\w{5,10}", "I really like strawberry"))
+print(re.search(r"s\w{,20}", "I really like strawberry"))
+
+#extractring process ID using regex
+log = "July 31 07:51:48 mycomputer bad_process[12345]: ERROR Performing package upgrade"
+regex = r"\[(\d+)\]"
+result = re.search(regex, log)
+print(result[1])
+result = re.search(regex, "A completely different string that also has numbers [34567]")
+print(result[1])
+
+
+def extract_pid(log_line):
+    regex = r"\[(\d+)\]" 
+    result = re.search(regex, log_line)
+    if result is None:
+        return ""
+    return result[1]
+
+print(extract_pid(log))
+print(extract_pid("99 elephants in a [cage]"))
+
+#split and replace
+print(re.split(r"[.?!]", "One sentence. Another one? And the last one!"))
+print(re.split(r"([.?!])", "One sentence. Another one? And the last one!"))
+print(re.sub(r"[\w.%+-]+@[\w.-]+", "[REDACTED]", "Received an email for go_nuts95@my.example.com"))
+print(re.sub(r"^([\w .-]), ([\w .-]*)$", r"\2\1" ,"Lovelace, Ada"))
